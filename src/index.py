fba44a6d7b50cc6bc4ba3e4b9ba3e6b32a03058a
Santeri Silvennoinen <santeri.silvennoinen@helsinki.fi>
2025-11-07 20:17:45 +0200

    code fixed after pylint badge tests

--- src/index.py
+++ src/index.py
@@ -13,20 +13,6 @@ def main():
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
-    # ohtuvarasto
-# ohtuvarasto
-
-![GHA workflow badge](https://github.com/serze10/ohtuvarasto/actions/workflows/main.yml/badge.svg)
-[![codecov](https://codecov.io/github/serze10/ohtuvarasto/graph/badge.svg?token=LY1T5DMW2Z)](https://codecov.io/github/serze10/ohtuvarasto)
-[![Pylint](https://github.com/santersi/ohtuvarasto/actions/workflows/pylint.yml/badge.svg?branch=main)](https://github.com/santersi/ohtuvarasto/actions/workflows/pylint.yml)
-
-
-
-![GHA workflow badge](https://github.com/serze10/ohtuvarasto/actions/workflows/main.yml/badge.svg)
-[![codecov](https://codecov.io/github/serze10/ohtuvarasto/graph/badge.svg?token=LY1T5DMW2Z)](https://codecov.io/github/serze10/ohtuvarasto)
-[![Pylint](https://github.com/santersi/ohtuvarasto/actions/workflows/pylint.yml/badge.svg?branch=main)](https://github.com/santersi/ohtuvarasto/actions/workflows/pylint.yml)
-
- 
 
 if __name__ == "__main__":
     main()
